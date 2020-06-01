de81b1069787ba829983fc492147c6f050a739b2
Pia Eichinger <pia.eichinger@hotmail.de>
2020-06-01 18:08:16 +0200

    bin: pasta_compare_getmaintainers: bulk mode and note path
    introducing bulk mode to choose multiple random message_ids at once
    and test them. Print the accepted/declined result.
    Introduing a note path to have all declined message_ids + declining
    reason noted for later checkup.
    
    Also doing some minor rewording and added a new regex
    
    Signed-off-by: Pia Eichinger <pia.eichinger@hotmail.de>

--- bin/pasta_compare_getmaintainers.py
+++ bin/pasta_compare_getmaintainers.py
@@ -12,6 +12,7 @@ from os import chdir, mkdir
 from pathlib import Path
 
 from pypasta.LinuxMaintainers import load_maintainers, LinuxSubsystem
+from pypasta.LinuxMailCharacteristics import load_linux_mail_characteristics
 
 log = getLogger(__name__[-15:])
 
@@ -45,10 +46,22 @@ def compare_getmaintainers(config, prog, argv):
                                                                             "Important: see to it that the mailboxes"
                                                                             " affected by the provided id's are "
                                                                             "active in the current Config")
+    parser.add_argument('--bulk', metavar='bulk', type=int, help="Bulk-Mode: If no message_id is provided, how many "
+                                                          "message_id\'s should be picked randomly and processed")
+
+    parser.add_argument('--notes_path', metavar='notes_path', type=str, help="Where to note erroneous message_id\'s")
 
     args = parser.parse_args(argv)
 
     victims = args.m_id
+    bulk = args.bulk
+    _notes_path = args.notes_path
+
+    def write_messageid_note(message_id, string):
+        if _notes_path is not None:
+            with open(_notes_path, "a+") as f:
+                if message_id not in f.read():
+                    f.write('\n' + message_id + " " + string)
 
     linusTorvaldsTuple = (
         'torvalds@linux-foundation.org', str(LinuxSubsystem.Status.Buried), "THE REST")
@@ -60,7 +73,13 @@ def compare_getmaintainers(config, prog, argv):
         all_message_ids = list(repo.mbox.get_ids(
             time_window=(config.mbox_mindate, config.mbox_maxdate),
             allow_invalid=False))
-        victims = [random.choice(all_message_ids)]
+        characteristics = load_linux_mail_characteristics(config, None, None, all_message_ids)
+        all_message_ids = [x for x in all_message_ids if
+                           characteristics[x].patches_linux]
+        if bulk is None:
+            victims = [random.choice(all_message_ids)]
+        else:
+            victims = random.sample(all_message_ids, bulk)
 
     tmp = defaultdict(list)
     for victim in victims:
@@ -78,6 +97,8 @@ def compare_getmaintainers(config, prog, argv):
         for file in linux_file_skeleton:
             Path(join(d_tmp, file)).touch()
 
+        accepted = 0
+        declined = 0
         for version, message_ids in victims.items():
             # build the structure anew for every different version
             repo_get_and_write_file(repo, version, "MAINTAINERS", d_tmp)
@@ -86,6 +107,25 @@ def compare_getmaintainers(config, prog, argv):
 
             for message_id in message_ids:
                 log.info("Processing %s (%s)" % (message_id, version))
+
+                message_raw = repo.mbox.get_raws(message_id)[0]
+                f_message = join(d_tmp, 'm')
+                with open(f_message, 'wb') as f:
+                    f.write(message_raw)
+
+                chdir(d_tmp)
+
+                pl_output = subprocess.run(
+                    ['perl ' + join(d_tmp, join("scripts", "get_maintainer.pl")) + ' '
+                     + f_message
+                     + ' --subsystem --status --separator \; --nogit --nogit-fallback --roles --norolestats']
+                    , shell=True
+                    , stdout=subprocess.PIPE).stdout.decode("utf-8")
+
+                if pl_output == "":
+                    log.error("perl script did not deliver any sort of output, nothing to compare. Exiting")
+                    return
+
                 patch = repo[message_id]
                 subsystems = linux_maintainers.get_subsystems_by_files(patch.diff.affected)
 
@@ -101,40 +141,21 @@ def compare_getmaintainers(config, prog, argv):
                     for maintainer in maintainers:
                         if len(subsystem_states) != 1:
                             log.error(
-                                "IMPORTANT: had more than one status or none? Lookup maintain_tuple " + str(maintainer)
-                                + " in retrieved " + str(maintainers))
-
+                                "maintainer for subsystem %s had more than one status or none? "
+                                "Lookup message_id %s" (subsystem, message_id))
+                        elif subsystem_states[0] is LinuxSubsystem.Status.Maintained:
                             status = "maintainer"
                         elif subsystem_states[0] is LinuxSubsystem.Status.Supported:
                             status = "supporter"
                         else:
-                            # probably not a good idea.
                             status = str(subsystem_states[0])
 
-                        to_be_appended = (maintainer[1].strip(), status, subsystem[0:40])
+                        to_be_appended = (maintainer[1].strip().lower(), status, subsystem[0:40])
 
                         if to_be_appended != linusTorvaldsTuple:
                             pasta_maintainers.append(to_be_appended)
 
                 log.info("maintainers successfully retrieved by PaStA")
-
-                message_raw = repo.mbox.get_raws(message_id)[0]
-                f_message = join(d_tmp, 'm')
-                with open(f_message, 'wb') as f:
-                    f.write(message_raw)
-
-                chdir(d_tmp)
-
-                pl_output = subprocess.run(
-                    ['perl ' + join(d_tmp, join("scripts", "get_maintainer.pl")) + ' '
-                     + f_message
-                     + ' --subsystem --status --separator \; --nogit --nogit-fallback --roles --norolestats']
-                     , shell=True
-                     , stdout=subprocess.PIPE).stdout.decode("utf-8")
-
-                if pl_output == "":
-                    log.error("perl script did not deliver any sort of output, nothing to compare. Exiting")
-                    return
 
                 pl_split = pl_output.split('\n')
                 pl_people = pl_split[0].split(';')
@@ -150,11 +171,17 @@ def compare_getmaintainers(config, prog, argv):
                 pasta_subsystems_abbrev = {subsystem[0:40] for subsystem in subsystems}
                 pl_subsystems_abbrev = {subsystem[0:40] for subsystem in pl_subsystems}
 
+                isAccepted = True
+
                 missing_subsys_pasta = pl_subsystems_abbrev - pasta_subsystems_abbrev
                 missing_subsys_pl = pasta_subsystems_abbrev - pl_subsystems_abbrev
                 if len(missing_subsys_pasta):
+                    isAccepted = False
+                    write_messageid_note(message_id, "Missing Subsystems in PaStA")
                     log.warning('Subsystems: Missing in PaStA: %s' % missing_subsys_pasta)
                 if len(missing_subsys_pl):
+                    isAccepted = False
+                    write_messageid_note(message_id, "Subsystems missin in get_maintainers")
                     log.warning('Subsystems: Missing in get_maintainers: %s' % missing_subsys_pl)
                 if pasta_subsystems_abbrev == pl_subsystems_abbrev:
                     log.info('Subsystems: Match')
@@ -163,8 +190,12 @@ def compare_getmaintainers(config, prog, argv):
                 missing_lists_pasta = pl_lists - pasta_lists
                 missing_lists_pl = pasta_lists - pasta_lists
                 if len(missing_lists_pasta):
+                    isAccepted = False
+                    write_messageid_note(message_id, "Missing List in PaStA")
                     log.warning('Lists: Missing in PaStA: %s' % missing_lists_pasta)
                 if len(missing_lists_pl):
+                    isAccepted = False
+                    write_messageid_note(message_id, "Missing List in get_maintainers")
                     log.warning('Lists: Missing in get_maintainers: %s' % missing_lists_pl)
                 if pl_lists == pasta_lists:
                     log.info('Lists: Match')
@@ -172,26 +203,43 @@ def compare_getmaintainers(config, prog, argv):
                 # Third, check if maintainers / reviewers / supports match. We now don't care
                 # about the subsystem any longer, but we do care about the state of the person
                 pl_person_regex = re.compile('.*<(.*)> \((.*):(.*)\)')
+                pl_system_regex = re.compile('(.*) \((.*):(.*)\)')
                 match = True
                 for pl_person in pl_people:
                     match = pl_person_regex.match(pl_person)
                     if not match:
-                        raise ValueError('regex did not match')
+                        write_messageid_note(message_id, "VALUE ERROR")
+                        # raise ValueError('regex did not match for person %s from message_id %s'
+                        #                 % (pl_person, message_id))
+                        match = pl_system_regex.match(pl_person)
+                        if not match:
+                            raise ValueError('regex did not match for person %s from message_id %s'
+                                             % (pl_person, message_id))
 
-                    triple = match.group(1), match.group(2), match.group(3)[0:40]
+                    triple = match.group(1).lower(), match.group(2).lower(), match.group(3)[0:40]
 
                     if triple in pasta_maintainers:
                         pasta_maintainers.remove(triple)
                     else:
+                        isAccepted = False
+                        write_messageid_note(message_id, "Missing entry for people in PaStA")
                         log.warning('People: Missing entry for %s (%s, %s)' % triple)
                         match = False
 
                 if len(pasta_maintainers):
+                    isAccepted = False
+                    write_messageid_note(message_id, "Too much entries in PaStA for people")
                     log.warning('People: Too much entries in PaStA: %s' % pasta_maintainers)
                     match = False
 
                 if match:
                     log.info('People: Match')
+                if isAccepted:
+                    accepted += 1
+                else:
+                    declined += 1
+        log.info("\nFrom a total of %s message_id\'s:\n%s passed comparison\n%s failed comparison"
+                 % (bulk, accepted, declined))
 
 
     finally:
